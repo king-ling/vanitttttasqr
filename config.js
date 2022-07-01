030a66928ed8f4753c27e4ec41a2cec601283b18
king_ling <108199084+king-ling@users.noreply.github.com>
2022-07-01 19:34:40 +0100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,366 @@
+const fs = require('fs')
+const chalk = require('chalk')
+
+global.thum = fs.readFileSync("./media/zaki.jpg")
+
+global.autoReadAll = true 
+
+// '6285878313791' ganti nomor kalian '628xxxxxx'
+//KiZakiXD ganti nama kalian xxxxz
+//Silahkan sesuaikan sendiri ya
+
+// Ganti Sewajarnya
+global.owner = ['6285878313791']
+global.pengguna = 'vanitasXD'
+global.premium = []
+global.author = 'vanitas bot'
+global.pemilik = ['6285878313791'] 
+global.ownernomer = '6285878313791'
+global.ownername = 'VANITAS'
+global.botname = 'VANITAS - MD'
+global.tengah = "									"
+global.Prefix = 'Multi Prefix'
+global.meki = '6285878313791'
+global.footer = 'V A N I T A S - 𝘔 𝘋'
+//Isi Sesuai Namalu Buat Tambah Tqtq
+global.namalu = 'Namalu' //Ubah nama kalian buat nambahin di tqtq bot
+global.footer1 = 'Jangan Spam Bot.'
+global.footer2 = 'Jika Menemukan Bug Lapor Ke Owner.'
+global.yt = 'https://instagrame/mohammed_xf16' //Jika tidak ada yt jangan diubah buat promosi owner xixixixi
+global.ig = 'https://instagrame/mohammed_xf16'
+global.gc = 'https://chat.whatsapp.com/IJ5ull4HXaY647D6ACw4kj' //join ya kadang kadang pada eval bot disitu lumayan dapet fitur baru wkwkwk
+global.dana = '00000'
+global.gopay = '0000'
+global.wame = 'wa.me/6285878313791'
+global.email = 'MOHAMMEDXDD16@gmail.com'
+global.region = 'morocoo'
+global.link = 'vanitas.bot'
+global.sc = 'https://instagrame/mohammed_xf16'
+global.packname = '"{\nAuthor: vanitas"\n"Bot Number: 6285848376737"\n"Bot Name: vanitas - MD"\n"Instagram: @mohammed_xf16"\n}' 
+global.author = '' 
+global.sessionName = 'session'
+global.prefa = ['#','!','/','°']
+global.sp = '❑ ' 
+global.mess = {
+    success: 'تمممممممممممممممممم',
+    admin: 'للمشرف فقططططططططط!',
+    botAdmin: 'أنا لست مشرف ',
+    owner: 'للمطور عمك فقط',
+    group: 'للمجموعات فقط!',
+    private: 'في الخاص فقط!',
+    bot: 'بووووووووووووووووووووووووووووووووووووووووووت',
+    wait: 'إنتضرررررررررررررر...',
+    error: 'خطأأأأأأأأأأأأأأأأأأأأأأأأأأأ',
+        endLimit: 'سببببباااااااام إنتضر حتى الغد أو بعد 12ساعة',
+    wrongFormat: 'أكتب جيدا!!\nعلامة 100000000000000000000000000في الإملاء',
+    example1: 'مرحبا @userفي مجموعة  @subject القوانين والمشرفين @desc\n\nNote :\n1. @user (Mention User Join)\n2. @subject (Group Name)\n3. @tanggal (Date Now)\n4. @desc (Get Description Group)'
+,
+    example2: 'Good Bye @user Di Group @subject Jangan Lupa Baca Rules @desc\n\nNote :\n1. @user (Mention User Join)\n2. @subject (Group Name)\n3. @tanggal (Date Now)\n4. @desc (Get Description Group)'    
+    
+}
+    global.limitawal = {
+    premium: "Unlimited",
+    free: 30,
+    monayawal: 1000
+}
+   global.rpg = {
+   darahawal: 100,
+   besiawal: 15,
+   goldawal: 10,
+   emeraldawal: 5,
+   umpanawal: 5,
+   potionawal: 1
+}
+global.thumb = fs.readFileSync('./media/zaki.jpg')
+global.fakeImg = fs.readFileSync('./media/fake.jpg')
+
+//Ambil dr chika
+global.flaming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.fluming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=fluffy-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.flarun = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=runner-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.flasmurf = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=smurfs-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+
+//Menu
+global.ccomannya = `
+  「 *تابعني على إنسغرام* 」
+ » mohammed_xf16
+  
+  「 *للمجموعات* 」
+ » linkgroup
+ » ephemeral [option]
+ » setppgc [image]
+ » setname [text]
+ » setdesc [text]
+ » group [option]
+ » editinfo [option]
+ » add @user
+ » kick @user
+ » hidetag [text]
+ » tagall [text]
+ » antilink [on/off]
+ » mute [on/off]
+ » promote @user
+ » demote @user
+ » vote [text]
+ » devote
+ » upvote
+ » cekvote
+ » hapusvote
+
+  「 *للمطور* 」
+ » react [emoji]
+ » chat [option]
+ » join [link]
+ » leave
+ » block @user
+ » setbiobot [text] 
+ » unblock @user
+ » bcgroup [text]
+ » bcall [text]
+ » setppbot [image]
+ » setexif
+  
+  「 *الترفيه* 」
+ » berburu
+ » mancing
+ » menambang
+ » mining
+ » heal
+ » profile
+ » inventory
+ » leaderboard
+ » buy
+ » sell
+
+  「 *التحميل* 」
+ » tiktok [url]
+ » instagram [url]
+ » twitter [url]
+ » facebook [url]
+ » igstory [query]
+ » umma [url]
+ » mediafire [url]
+ » gitclone [url]
+ 
+  「 *شعارات* 」
+ » textmaker glicth [Text]
+ » textmaker glow [Text]
+ » flasmurf [Text]
+ » flarun [Text]
+ » fluming [Text]
+ » flaming [Text]
+ » lava [Text]
+ » neonlight [Text]
+ » fabric [Text]
+ » glue [Text]
+ » luxury [Text]
+ » underwater [Text]
+ » harrypotter [Text]
+ » steel [Text]
+ » metallic [Text]
+ » graffiti [Text]
+ » pencil [Text]
+ » sand [Text]
+ » magma [Text]
+ » joker [Text]
+ » sky [Text]
+ » matrix [Text]
+ » neon [Text]
+ » blackpink [Text]
+ » thunder [Text]
+ » whitebear [Text]
+ » hoorror [Text]
+ » 8bit [Text]
+ » horror [Text]
+ » retro [Text]
+ » pornhub [Text]
+ » juice [Text]
+ » batman [Text]
+ » multicolor [Text]
+ » collwall [Text]
+ » cool [Text]
+ » wonderful [Text]
+ » sketch [Text]
+ » skeleton [Text]
+ » marvel [Text]
+ » foggy [Text]
+ » writing [Text]
+ » halloween [Text]
+ » watercolor [Text]
+ » classic [Text]
+ » rainbow [Text]
+ » sci_fi [Text]
+ » christmas [Text]
+ » ancient [Text]
+ » toxic [Text]
+
+  「 *Stalker Menu* 」
+ » ghstalk [query]
+ » igstalk [query]
+ » ffstalk [query]
+ » supersusstalk [query]
+
+  「 *الأنترنيت* 」
+ » ip [query]
+ » iploc [query]
+ » ipinfo [query]
+ » ssweb [query]
+  
+  「 *البحث* 」
+ » play [query]
+ » yts [query]
+ » google [query]
+ » gimage [query]
+ » pinterest [query]
+ » wallpaper [query]
+ » wikimedia [query]
+ » ytsearch [query]
+ » ringtone [query]
+ » stalk [option] [query]
+ » webtoons [query]
+ » drakor [query]
+ » anime [query]
+ » character [query]
+ » manga [query]
+
+  「 *الترفيييييه2* 」
+ » coffe
+ » quotesanime
+ » couple
+ » anime
+ » waifu (nsfw)
+ » husbu (nsfw)
+ » neko (nsfw)
+ » shinobu (nsfw)
+ » waifus (nsfw)
+ » nekos (nsfw)
+ » trap (nsfw)
+ » blowjob (nsfw)
+
+  「 *أنمييييي* 」
+ » loli
+ » neko
+ » waifu
+ » shinobu
+ » megumin
+ » bully
+ » cuddle
+ » cry
+ » hug
+ » awoo
+ » kiss
+ » lick
+ » pat
+ » smug
+ » bonk
+ » yeet
+ » blush
+ » smile
+ » wave
+ » highfive
+ » handhold
+ » nom
+ » bite
+ » glomp
+ » slap
+ » kill
+ » happy
+ » wink
+ » poke
+ » dance
+ » cringe
+
+  「 *الترفيه 3* 」
+ » bagaimanakah
+ » kapankah
+ » apakah
+ » bisakah
+ » rate
+ » wangy
+ » gantengcek
+ » cekganteng
+ » cantikcek
+ » cekcantik
+ » sangecek
+ » ceksange
+ » cekme
+ » gaycek
+ » cekgay
+ » lesbicek
+ » halah
+ » hilih
+ » huluh
+ » heleh
+ » holoh
+ » jadian
+ » jodohku
+ » delttt
+ » tictactoe
+ » family100
+ » tebak [option]
+ » math [mode]
+ » suitpvp [@tag]
+
+  「 *تحويل* 」
+ » toimage
+ » removebg
+ » sticker
+ » emojimix
+ » tovideo
+ » togif
+ » tourl
+ » tovn
+ » tomp3
+ » toaudio
+ » ebinary
+ » dbinary
+ » styletext
+ » smeme
+
+  「 *Database Menu* 」
+ » setcmd
+ » listcmd
+ » delcmd
+ » lockcmd
+ » addmsg
+ » listmsg
+ » getmsg
+ » delmsg
+
+  「 *المجهوووووول* 」
+ » anonymous
+ » start
+ » next
+ » keluar
+ » sendkontak
+
+  「 *أفضل أوامر * 」
+ » iqra
+ » hadist
+ » alquran
+ » juzamma
+ » tafsirsurah
+
+  「 *تغيير الصوت* 」
+ » bass
+ » blown
+ » deep
+ » earrape
+ » fast
+ » fat
+ » nightcore
+ » reverse
+ » robot
+ » slow
+ » tupai
+`
+global.sewanya =
+'  *» تابعني :*\n تابعني على إنستغرام : 7K_\n   _• mohammed_xf16 : 15K_\n   _•https://instagrame/mohammed_xf16_'
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Update'${__filename}'`))
+	delete require.cache[file]
+	require(file)
+})
